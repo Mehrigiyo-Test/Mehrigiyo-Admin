8ba144fcf6736d1a70973729e564949159213e81
Xurshid <nematoff4744@gmail.com>
2022-04-23 04:19:54 +0500

    update

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import BottomHeader from "./components/BottomHeader";
 import TopHeader from "./components/TopHeader";
+import styles from "./Header.module.scss"
 const Header = () => {
   return (
-    <div>
+    <div className={styles.container} >
       <TopHeader />
       <BottomHeader />
     </div>
