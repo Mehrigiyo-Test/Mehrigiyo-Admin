3c8a49310ff98df365660f448e9edbb449577495
Xurshid <nematoff4744@gmail.com>
2022-04-23 22:23:33 +0500

    control panel finished

--- src/pages/ControlPanel/ControlPanel.jsx
+++ src/pages/ControlPanel/ControlPanel.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import styles from "./ControlPanel.module.scss";
-import Title from "../../components/Title/Title";
 
-import {ReactComponent as BoldArrow} from '../../icons/BoldArrow.svg'
+import Title from "../../components/Title/Title";
+import VideoCallBox from "./_components/VideoCallBox";
+import LastNoticeBox from "./_components/LastNoticeBox";
+import TableCard from "./_components/TableCard";
+
+import { doctors } from "../../constants/doctors";
+import { ReactComponent as BoldArrow } from "../../icons/BoldArrow.svg";
+import leafs from "../../images/Leafs.png";
+import NoUpcoming from "../../images/NoUpcoming.png";
+
 const ControlPanel = () => {
+  const arr = [
+    {
+      title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+      subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ...",
+    },
+    {
+      title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+      subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ...",
+    },
+  ];
   return (
     <>
       <section className={styles.container}>
@@ -13,18 +31,97 @@ const ControlPanel = () => {
             subtitle={"Bugun bemorlarimizga xizmat qilish uchun ajoyib kun."}
           />
         </div>
-        <div>
+        <div className={styles.wrapper}>
           <div>
             <span className={styles.nextTable}>
               <h4>Kelgusi jadval</h4>
               <span>
                 <p>Hammasi</p>
-                <BoldArrow/>
+                <BoldArrow />
               </span>
             </span>
+            <section className={styles.box}>
+              <div>
+                {doctors
+                  .filter((item) => item.name === "Gavhar")
+                  .map((item, index) => (
+                    <VideoCallBox
+                      name={item.name}
+                      surname={item.surname}
+                      img={item.img}
+                      profession={item.profession}
+                      key={index}
+                    />
+                  ))}
+              </div>
+              <div>
+                <LastNoticeBox
+                  title={"Bepul maslahat olish"}
+                  subtitle={"Savolaringizga javob berishadi."}
+                  bgColor={"#F5F0EC"}
+                  color={"#C88D5E"}
+                />
+              </div>
+              <br />
+              <hr style={{ opacity: "0.3" }} />
+            </section>
+            <span className={styles.nextTable}>
+              <h4>Oxirgi bildirishnoma</h4>
+              <span>
+                <p>Hammasi</p>
+                <BoldArrow />
+              </span>
+            </span>
+            <span className={styles.date}>Today - 12:00</span>
+            <div>
+              {arr.map((item) => (
+                <LastNoticeBox title={item.title} subtitle={item.subtitle} />
+              ))}
+            </div>
+            <span className={styles.date}>28/01/2022 - 12:00</span>
+            <div>
+              {arr.map((item) => (
+                <LastNoticeBox title={item.title} subtitle={item.subtitle} />
+              ))}
+            </div>
           </div>
 
-          <div></div>
+          <div className={styles.importantTable}>
+            <h4>Muhim jadval</h4>
+            {doctors
+              .filter((a) => a.name === "Bahrom")
+              .map((item) => (
+                <TableCard
+                  name={item.name}
+                  surname={item.surname}
+                  profession={item.profession}
+                  img={item.img}
+                />
+              ))}
+            <img
+              src={leafs}
+              alt=""
+              width={225}
+              height={305}
+              className={styles.leafs}
+            />
+            <section className={styles.noData}>
+              <img
+                src={NoUpcoming}
+                alt=""
+                className={styles.noUpcoming}
+                width={130}
+                height={130}
+              />
+              <div className={styles.titles}>
+                <h4>No upcoming consultation</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur</p>
+              </div>
+              <div className={styles.btn}>
+                <p>Let’s find your doctor 😷</p>
+              </div>
+            </section>
+          </div>
         </div>
       </section>
     </>
