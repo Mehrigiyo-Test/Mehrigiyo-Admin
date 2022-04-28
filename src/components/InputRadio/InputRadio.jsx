c53abe8158943fb2a608b0e56553c65f00f1aaad
Alisher Umarov <umarovalisher132@gmail.com>
2022-04-28 14:12:27 +0500

    fixed bugs

--- src/components/InputRadio/InputRadio.jsx
+++ src/components/InputRadio/InputRadio.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./InputRadio.scss";
 
-const Inputradio = ({ title, subtitle, icon ,id }) => {
+const Inputradio = ({ title, subtitle, icon, id}) => {
   return (
     <>
-      <div className="del-address">
+      <label className="del-address">
         <div className="del-address__input">
           <div className="del-address__input__wrapper">
-            <input type="radio" className="checkbox" id={id} name="input"/>
+            <input type="radio" className="appearance-none rounded-full h-5 w-5 border-2 border-gray-300 bg-white checked:bg-[#53B175] checked:border-white focus:outline-none transition duration-200 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" id={id} name="input" />
             <div className="del-address__titles">
               <h2 className="del-address__titles__title">{title}</h2>
               <p className="del-address__titles__text">{subtitle}</p>
@@ -18,7 +18,7 @@ const Inputradio = ({ title, subtitle, icon ,id }) => {
             <img src={icon} alt="rasm" />
           </span>
         </div>
-      </div>
+      </label>
     </>
   );
 };
